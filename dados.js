7a942bc3089fd4e70c588904fc89a3225e94b46e
ClaudioCesar1502 <claudioc.alves@hotmail.com>
2024-09-11 16:28:40 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -3,18 +3,42 @@ let dados = [
         titulo : "BPC",
         descricao : "O Benefício de Prestação Continuada (BPC) é um auxílio financeiro concedido pelo governo a pessoas com deficiência de qualquer idade e a idosos com 65 anos ou mais que comprovem não ter condições de se manter ou de serem mantidos por sua família",
         link : "https://www.gov.br/mds/pt-br/acoes-e-programas/suas/beneficios-assistenciais/beneficio-assistencial-ao-idoso-e-a-pessoa-com-deficiencia-bpc",
-        tags: "beneficio Bene bene pcd idoso"
+        tags: "beneficio bene ben prestacao bpc auxlio  a  bb  f d x c q m  h j  k "
     },
     {
         titulo : "Auxilio Inclusao",
         descricao : "O Auxílio Inclusão é um benefício assistencial destinado a pessoas com deficiência que necessitam de apoio para sua inclusão social. Ele visa garantir o acesso a serviços como educação, saúde e trabalho, promovendo a autonomia e a independência dessas pessoas.",
         link : "https://www.gov.br/mds/pt-br/acoes-e-programas/suas/beneficios-assistenciais/auxilio-inclusao",
-        tags: "inclu inclusao inclusão assis auxilio"
+        tags: "beneficio bene ben prestacao inclusao in auxlio  a  bb  f d x c q m  h j  k"
     },
     {
         titulo : "RMV",
         descricao : "A Renda Mensal Vitalícia é um benefício assistencial destinado a pessoas com deficiência grave ou profunda que não possuem condições de trabalhar e nem de ter sua subsistência e cuidados garantidos por sua família. O valor do benefício varia de acordo com a gravidade da deficiência.",
         link : "https://www.gov.br/mds/pt-br/acoes-e-programas/suas/beneficios-assistenciais/renda-mensal-vitalicia",
-        tags: "vital vitalicia assistencial "        
+        tags: "beneficio bene ben prestacao rmv auxlio  a  bb  f d x c q m  h j  k"        
+    },
+    {
+        titulo : "Bolsa Familia",
+        descricao : "A bolsa familia, atende a famílias que vivem em situação de pobreza e de extrema pobreza. Há limites de renda para definir essas duas situações. As famílias atendidas pelo programa recebem um benefício mensal em dinheiro, que é transferido diretamente pelo governo federal, e devem cumprir alguns compromissos que têm como objetivo reforçar o acesso à educação, à saúde e à assistência social.",
+        link : "https://www.gov.br/mds/pt-br/acoes-e-programas/bolsa-familia",
+        tags: "beneficio bene ben prestacao familia bolsa auxlio  a  bb  f d x c q m  h j  k"        
+    },
+    {
+        titulo : "Garantia-Safra",
+        descricao : "Garantia-safra é uma ação do Programa Nacional de Fortalecimento da Agricultura Familiar (Pronaf). Possui como beneficiários agricultores que têm renda familiar mensal de, no máximo, 1,5 (um e meio) salário mínimo e que plantam entre 0,6 e 5 hectares de feijão, milho, arroz, mandioca e algodão. Uma vez aderidos ao programa, eles passam a receber o benefício quando o município em que moram comprova a perda de, pelo menos, 50% do conjunto dessas produções, ou de outras a serem definidas pelo órgão gestor do Fundo Garantia-Safra, em razão de estiagem ou excesso hídrico.",
+        link : "https://www.gov.br/pt-br/servicos/consultar-o-garantia-safra",
+        tags: "beneficio bene ben prestacao araia safra rantia auxlio  a  bb  f d x c q m  h j  k"        
+    },
+    {
+        titulo : "Seguro-Defeso (Pescador Artesanal)",
+        descricao : "O seguro-desemprego do pescador artesanal é uma assistência financeira temporária concedida aos pescadores profissionais artesanais que, durante o período de defeso (aquele em que os animais protegidos normalmente se reproduzem), são obrigados a paralisar a atividade para preservação da espécie e sustentabilidade da atividade pesqueira na região. Para ter direito, o trabalhador deve comprovar que exerce a pesca, do pescado protegido, de maneira ininterrupta, seja sozinho ou em regime de economia familiar.",
+        link : "https://www.gov.br/pt-br/servicos/solicitar-seguro-defeso-pescador-artesanal",
+        tags : "beneficio bene ben prestacao defeso seguro se auxlio  a  bb  f d x c q m  h j  k" 
+    },
+    {
+        titulo : "Programa de Erradicação do Trabalho Infantil (PETI)",
+        descricao : "Conjunto de ações que tem o objetivo de retirar crianças e adolescentes menores de 16 anos do trabalho precoce, exceto na condição de aprendiz a partir de 14 anos. O programa, além de assegurar transferência direta de renda a famílias, oferece a inclusão de crianças e jovens em serviços de orientação e de acompanhamento. A frequência à escola também é exigida.",
+        link: "https://www.gov.br/mds/pt-br/acoes-e-programas/suas/servicos-e-programas/acao-estrategica-do-programa-de-erradicacao-do-trabalho-infantil#:~:text=Em%202011%2C%20o%20PETI%20foi%20institu%C3%ADdo%20pela%20Lei,adolescentes%20que%20se%20encontram%20em%20situa%C3%A7%C3%A3o%20de%20trabalho.",
+        tags : "beneficio bene ben erra prestacao peti seguro se auxlio  a  bb  f d x c q m  h j  k"
     }
 ];
